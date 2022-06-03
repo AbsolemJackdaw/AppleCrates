c99c96c41048d70e5389041c6afaa74aec66ce6a
Subaraki <robbeenaxel@hotmail.com>
2022-06-03 16:57:25 +0200

    data now saved in block entity compoundtag

--- Forge/src/main/java/jackdaw/applecrates/block/blockentity/CrateBE.java
+++ Forge/src/main/java/jackdaw/applecrates/block/blockentity/CrateBE.java
@@ -1,8 +1,13 @@
 package jackdaw.applecrates.block.blockentity;
 
+import jackdaw.applecrates.container.CrateStackHandler;
 import jackdaw.applecrates.registry.GeneralRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.Connection;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.WoodType;
@@ -10,8 +15,8 @@ import net.minecraftforge.items.ItemStackHandler;
 
 public class CrateBE extends BlockEntity {
 
-    public ItemStackHandler crateInventory = new ItemStackHandler(30);
-    public ItemStackHandler crateSales = new ItemStackHandler(2);
+    public CrateStackHandler crateStock = new CrateStackHandler();
+    public ItemStackHandler interactable = new ItemStackHandler(2);
     public ItemStackHandler priceAndSale = new ItemStackHandler(2);
 
     public CrateBE(WoodType type, BlockPos pos, BlockState state) {
@@ -22,11 +27,19 @@ public class CrateBE extends BlockEntity {
     public void load(CompoundTag tag) {
         super.load(tag);
 
+        crateStock.deserializeNBT((CompoundTag) tag.get("cratestock"));
+        interactable.deserializeNBT((CompoundTag) tag.get("interactable"));
+        priceAndSale.deserializeNBT((CompoundTag) tag.get("pricensale"));
+
     }
 
     @Override
     protected void saveAdditional(CompoundTag tag) {
         super.saveAdditional(tag);
-    }
 
+        tag.put("cratestock", crateStock.serializeNBT());
+        tag.put("interactable", interactable.serializeNBT());
+        tag.put("pricensale", priceAndSale.serializeNBT());
+
+    }
 }
