e4b7fbc0693ef7a341080d7409995a84c2815c2a
Subaraki <robbeenaxel@hotmail.com>
2022-06-05 19:00:58 +0200

    fix render item rotation not applying correctly with transforms

--- Forge/src/main/java/jackdaw/applecrates/client/besr/CrateBESR.java
+++ Forge/src/main/java/jackdaw/applecrates/client/besr/CrateBESR.java
@@ -25,15 +25,39 @@ public class CrateBESR implements BlockEntityRenderer<CrateBE> {
     public void render(CrateBE pBlockEntity, float pPartialTick, PoseStack stack, MultiBufferSource pBufferSource, int pPackedLight, int pPackedOverlay) {
 
         Vec3 randomOffset = calculateOffset(pBlockEntity.getBlockPos());
-        Quaternion blockRotation = pBlockEntity.getBlockState().getValue(CrateBlock.FACING).getRotation();
+        float blockRotation = pBlockEntity.getBlockState().getValue(CrateBlock.FACING).toYRot();
         ItemStack selling = pBlockEntity.priceAndSale.getStackInSlot(1);
 
         if (!selling.isEmpty())
             for (int i = 0; i < 3; i++) {
                 stack.pushPose();
-                stack.translate(0.5F + (i == 2 ? randomOffset.x() : 0.0), 0.3F + (0.02f * i), 0.6F + (i == 1 ? randomOffset.z() : 0));
-                stack.mulPose(blockRotation);
-                stack.mulPose(POS_OFFSET);
+
+                //prepare normalisation of crate rotation in shown itemstacks
+                int angleSimp = (int) pBlockEntity.getBlockState().getValue(CrateBlock.FACING).toYRot() / 90;
+                float xoff = angleSimp == 1 || angleSimp == 2 ? 1.0f : 0.0f;
+                float zoff = angleSimp == 2 || angleSimp == 3 ? 1.0f : 0.0f;
+                float zfront = angleSimp % 2 == 1 ? (0.5f * (angleSimp == 3 ? -1 : 1)) : 0f;
+                float xfront = angleSimp % 2 == 0 ? (0.5f * (angleSimp == 2 ? -1 : 1)) : 0f;
+
+                stack.translate(xoff + xfront, 0, zoff + zfront);
+
+                float xAngle = (90.0f - 22.5f) * (xfront == 0 ? (zfront * (zfront < 0 ? 2f : -2f)) : (xfront * (xfront < 0 ? 2f : -2f)));
+
+                stack.mulPose(new Quaternion(0f, blockRotation + (angleSimp % 2 == 0 ? 180.0f : 0f), 0f, true));
+                stack.mulPose(new Quaternion(xAngle, 0, 0, true));//do not merge quaternions
+
+                /////////////do actual translation or offset here./////////////
+                //translate is z,x,y
+                //or crate's left/right, up/down, and lower/higher
+                float randX = (float) calculateOffset(pBlockEntity.getBlockPos()).x();
+                float randZ = (float) calculateOffset(pBlockEntity.getBlockPos()).z();
+
+                stack.translate(
+                        (i == 0 ? 0.0f : randX / (float) i * (i == 1 ? -1 : 1)), // x or crate's left/right
+                        0.25f + (i == 0 ? 0.0f : randZ / (float) i), //z or crate's up/down
+                        0.1f + (float) i * 0.025 //y or crate's higher/lower. In general, don't touch this value
+                );
+
                 Minecraft.getInstance().getItemRenderer().renderStatic(
                         selling,
                         ItemTransforms.TransformType.GROUND,
@@ -49,9 +73,9 @@ public class CrateBESR implements BlockEntityRenderer<CrateBE> {
 
     private Vec3 calculateOffset(BlockPos pPos) {
         long i = Mth.getSeed(pPos.getX(), 0, pPos.getZ());
-        float horizontalOffset = 0.8f;
-        double d0 = Mth.clamp((((i & 15L) / 15.0F) - 0.5D) * 0.5D, (-horizontalOffset), horizontalOffset);
-        double d2 = Mth.clamp((((i >> 8 & 15L) / 15.0F) - 0.5D) * 0.5D, (-horizontalOffset), horizontalOffset);
+        float horizontalOffset = 0.2f;
+        double d0 = Mth.clamp((((i & 15L) / 15.0F)) * 0.5D, (-horizontalOffset), horizontalOffset);
+        double d2 = Mth.clamp((((i >> 8 & 15L) / 15.0F)) * 0.5D, 0, 0.4);
         return new Vec3(d0, 0.0, d2);
     }
 }
