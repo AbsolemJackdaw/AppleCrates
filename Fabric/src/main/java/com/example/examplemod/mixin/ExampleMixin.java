6a0c61151c36f3e54bcd741cdc882e0a8237da47
Subaraki <robbeenaxel@hotmail.com>
2022-06-05 15:37:25 +0200

    empty fabric directory of unneeded code

--- Fabric/src/main/java/com/example/examplemod/mixin/ExampleMixin.java
+++ Fabric/src/main/java/com/example/examplemod/mixin/ExampleMixin.java
@@ -1,6 +1,5 @@
 package com.example.examplemod.mixin;
 
-import com.example.examplemod.Constants;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,8 +13,5 @@ public class ExampleMixin {
     @Inject(at = @At("HEAD"), method = "init()V")
     private void init(CallbackInfo info) {
         
-        Constants.LOG.info("This line is printed by an example mod mixin from Fabric!");
-        Constants.LOG.info("MC Version: {}", Minecraft.getInstance().getVersionType());
-        Constants.LOG.info("Classloader: {}", this.getClass().getClassLoader());
     }
 }
