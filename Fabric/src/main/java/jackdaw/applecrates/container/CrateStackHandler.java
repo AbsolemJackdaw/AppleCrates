e9748a14964e9fe52d23d75381f6d8ac3b5fe83e
Subaraki <robbeenaxel@hotmail.com>
2022-10-28 10:44:27 +0200

    remove duplicate code

--- Fabric/src/main/java/jackdaw/applecrates/container/CrateStackHandler.java
+++ Fabric/src/main/java/jackdaw/applecrates/container/CrateStackHandler.java
@@ -59,33 +59,4 @@ public class CrateStackHandler extends SimpleContainerNBT {
         setItem(29, prepXchange);
         return true;
     }
-
-    public void deserializeNBT(CompoundTag nbt) {
-        //setSize(nbt.contains("Size", Tag.TAG_INT) ? nbt.getInt("Size") : items.size());
-        ListTag tagList = nbt.getList("Items", Tag.TAG_COMPOUND);
-        for (int i = 0; i < tagList.size(); i++) {
-            CompoundTag itemTags = tagList.getCompound(i);
-            int slot = itemTags.getInt("Slot");
-
-            if (slot >= 0 && slot < items.size()) {
-                items.set(slot, ItemStack.of(itemTags));
-            }
-        }
-    }
-
-    public CompoundTag serializeNBT() {
-        ListTag nbtTagList = new ListTag();
-        for (int i = 0; i < getContainerSize(); i++) {
-            if (!items.get(i).isEmpty()) {
-                CompoundTag itemTag = new CompoundTag();
-                itemTag.putInt("Slot", i);
-                items.get(i).save(itemTag);
-                nbtTagList.add(itemTag);
-            }
-        }
-        CompoundTag nbt = new CompoundTag();
-        nbt.put("Items", nbtTagList);
-        nbt.putInt("Size", items.size());
-        return nbt;
-    }
 }
